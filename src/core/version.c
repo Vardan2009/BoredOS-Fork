b4c14af48d17ce1495074ffff559aa0961b55657
boreddevnl <chris@boreddev.nl>
2026-04-12 17:56:35 +0200

    TWEAK: kernel_version = 3.2.3 --> 4.0.0

--- src/core/version.c
+++ src/core/version.c
@@ -16,7 +16,7 @@ void get_os_info(os_info_t *info) {
     const char *os_version = "26.4";
     const char *os_codename = "Geometry";
     const char *kernel_name = "Boredkernel";
-    const char *kernel_version = "3.2.3";
+    const char *kernel_version = "4.0.0";
     const char *build_date = __DATE__;
     const char *build_time = __TIME__;
     const char *build_arch = "x86_64";
